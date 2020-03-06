adc47e991043b89a15b48f1cf58ecf1ce660fc24
Nisha Garg <14gargnisha@gmail.com>
2020-03-06 21:37:55 +0530

    committing Linked List insert and delete

--- src/com/java/linkedlist/ListUtility.java
+++ src/com/java/linkedlist/ListUtility.java
@@ -5,35 +5,35 @@ public class ListUtility {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to SinglyLinkedList Program");
-		System.out.println("Enter Y to continue and N to exit");
-		
+		System.out.println("Press Y to continue and N to exit");
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
 		str= str.toUpperCase();
+		SinglyLinkedList slist = new SinglyLinkedList();
 		do {
 			switch (str) {
-			case "Y": {
-				getInput();
+			case "Y": 
+				getInput(slist);
 				System.out.println("Do you want to continue? Press Y to continue and N to exit");
 				str = sc.next();
+				str= str.toUpperCase();
 				break;
-			}
-			case "N": {
+			case "N": 
+				System.out.println("Program has been terminated successfully.");
 				System.exit(0);
 				break;
-			}
-			default: {
+			default:
 				System.out.println("Please enter the valid input, Press Y to continue and N to exit");
 				str = sc.next();
-			}
+				str= str.toUpperCase();
 			}
 		} while (!str.equals("N"));
-
+		System.out.println("Program has been terminated successfully.");
 	}
 
-	private static void getInput() {
+	private static void getInput(SinglyLinkedList slist) {
 		Scanner sc = new Scanner(System.in);
-		int index;
+		int index, data;
 		System.out.println("Linked List Operations are: ");
 		System.out.println("Insert");
 		System.out.println("Delete");
@@ -42,20 +42,57 @@ public class ListUtility {
 		System.out.println("Print");
 		System.out.println("enter the operation name which you want to perform");
 		String operation = sc.next().toLowerCase();
-		SinglyLinkedList slist = new SinglyLinkedList();
+		
 		
 		switch(operation) {
-			case "insert": {
+			case "insert":
 				System.out.println("Enter the index where you want to insert new node: ");
 				index = sc.nextInt();
-				if(index < 0 || index == 0) {
-					slist.insertAtBeginning();
-				} else if(index > slist.getListLength() || index == slist.getListLength()) {
-					slist.insertAtEnd();
+				System.out.println("Enter the data for node: ");
+				data = sc.nextInt();
+				if(index <= 0) {
+					slist.insertAtBeginning(data);
+				} else if(index >= slist.getListLength()) {
+					slist.insertAtEnd(data);
 				} else {
-					slist.insertAtIndex(index);
+					slist.insertAtIndex(index, data);
 				}
-			}
+				break;
+			case "print":
+				System.out.println("Linked List is: ");
+				slist.printLinkedList();
+				break;
+			case "count":
+				System.out.println("Total number of elements in the Linked list are: " + slist.getListLength());
+			case "delete":
+				System.out.println("Enter 'index' to delete node using index and 'data' to delete node as per the data");
+				String type = sc.next().toLowerCase();
+				
+				switch(type) {
+					case "index":
+						System.out.println("Enter the index of node which you want to delete: ");
+						index = sc.nextInt();
+						if(index <= 0) {
+							slist.deleteFromBeginning();
+						} else if(index >= slist.getListLength()) {
+							slist.deleteFromEnd();
+						} else {
+							slist.deleteForGivenIndex(index);
+						}
+						break;
+					case "data":
+						System.out.println("Enter the data which you want to delete");
+						data = sc.nextInt();
+						slist.deleteForGivenData(data);
+						break;
+					default:
+						System.out.println("Invalid Input");
+				}
+				break;
+				
+			default: 
+				System.out.println("Wrong Operation name has been entered");
+				
 		} 
 		
  	}
--- src/com/java/linkedlist/SinglyLinkedList.java
+++ src/com/java/linkedlist/SinglyLinkedList.java
@@ -3,7 +3,7 @@ package com.java.linkedlist;
 
 public class SinglyLinkedList {
     ListNode head = null;
-	
+	// to get the length of the list
 	public int getListLength() {
 		ListNode currentNode = head;
 		int length = 0;
@@ -14,19 +14,134 @@ public class SinglyLinkedList {
 		return length;
 	}
 
-	public void insertAtBeginning() {
-		// TODO Auto-generated method stub
+	// to insert the new node at the beginning
+	public void insertAtBeginning(int data) {
+		ListNode node = new ListNode(data);
+		if(head != null) {
+			node.setNext(head);
+		}
+		head = node;
+	}
+
+	// to insert the new node at the end
+	public void insertAtEnd(int data) {
+		ListNode node = new ListNode(data);
+		ListNode currentNode = head;
+		if(head == null) {
+			head = node;
+		} else {
+			while(currentNode.getNext() != null) {
+				currentNode = currentNode.getNext();
+			}
+			currentNode.setNext(node);
+		}
+	}
+
+	// to insert a new node a particular index given
+	public void insertAtIndex(int index, int data) {
+		ListNode node = new ListNode(data);
+		int pos = 0;
+		ListNode currentNode = head;
+		while(currentNode != null) {
+			
+			if(pos == index) {
+				node.setNext(currentNode.getNext());
+				currentNode.setNext(node);
+				break;
+			}
+			currentNode = currentNode.getNext();
+			pos ++;
+		}
+	}
+
+	// to print the values of Linked list
+	public void printLinkedList() {
+		ListNode currentNode = head;
+		System.out.print((head == null) ? "OOPS! Currently Linked List is empty: [] \n" : "[");;
+
+		while(currentNode != null) {
+			if(currentNode.getNext() != null) {
+				System.out.print(currentNode.getData() + ", ");
+			} else {
+				System.out.print(currentNode.getData() + "]\n");
+			}
+			currentNode = currentNode.getNext();
+		}
+		
 		
 	}
 
-	public void insertAtEnd() {
-		// TODO Auto-generated method stub
-		
+	// to delete node from the beginning
+	public void deleteFromBeginning() {
+		if(head == null) {
+			System.out.println("Sorry! Nothing to delete in the Linked List");
+		} else {
+			ListNode tempNode = head.getNext();
+			head = tempNode;
+		}
+		System.out.println("First Node has been deleted successfully");
 	}
 
-	public void insertAtIndex(int index) {
-		// TODO Auto-generated method stub
-		
+	// to delete the node from end
+	public void deleteFromEnd() {
+		if(head == null) {
+			System.out.println("Sorry! Nothing to delete in the Linked List");
+		} else {
+			ListNode currentNode = head;
+			ListNode previousNode = null;
+			while(currentNode.getNext() != null) {
+				previousNode = currentNode;
+				currentNode = currentNode.getNext();
+			}
+			previousNode.setNext(null);
+		}
+		System.out.println("Last Node has been deleted succussfully");
+	}
+
+	// to delete the node for the given index
+	public void deleteForGivenIndex(int index) {
+		ListNode currentNode = head;
+		ListNode previousNode = null;
+		int pos = 0;
+		while(currentNode != null) {
+			if(pos == index) {
+				previousNode.setNext(currentNode.getNext());
+				break;
+			}
+			previousNode = currentNode;
+			currentNode = currentNode.getNext();
+			pos ++;
+		}
+		System.out.println("Node for index = " + index + " has been deleted succussfully");
+	}
+
+	// to delete the node for the first matched occurrence of data
+	public void deleteForGivenData(int data) {
+		ListNode currentNode = head;
+		ListNode previousNode = null;
+		boolean hasMatchFound = false;
+		if(head == null) {
+			System.out.println("Sorry! Nothing to delete in the Linked List");
+		} else {
+			while(currentNode != null) {
+				if(currentNode.getData() == data) {
+					hasMatchFound = true;
+					if(previousNode == null) {
+						head = currentNode.getNext();
+					} else {
+						previousNode.setNext(currentNode.getNext());
+					}
+					break;
+				}
+				previousNode = currentNode;
+				currentNode = currentNode.getNext();
+			}
+			if(hasMatchFound) {
+				System.out.println("Node with data = " + data + " has been deleted successfully.");
+			} else {
+				System.out.println("Sorry! Given data is not present in the list");
+			}
+		}
 	}
 	
 }
